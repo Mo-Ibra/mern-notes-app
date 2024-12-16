1b926fc78ec64467153c22d9bd606069662a2f5c
Mo-Ibra <mohamedelgedwy245@gmail.com>
2024-12-16 21:16:29 +0200

    Added ViewNote JSX

--- frontend/src/pages/ViewNote/ViewNote.jsx
+++ frontend/src/pages/ViewNote/ViewNote.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
 const ViewNote = () => {
+  const { id } = useParams();
+
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      const response = await fetch(`http://localhost:5000/api/notes/${id}`);
+
+      if (response.ok) {
+        const note = await response.json();
+        console.log(note);
+
+        setNote({ title: note.title, content: note.content });
+      }
+    };
+
+    fetchNote();
+  }, [id]);
+
   return (
-    <div>
-      <h1>View Note Page</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">{note.title}</h1>
+      <p className="mb-4">{note.content}</p>
+      <Link to="/" className="bg-gray-500 text-white px-4 py-2 rounded">
+        Go Back
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default ViewNote;
